f73b746019a7c233371c8c7fbc166c8b072f8822
Dezziezuok <dezzie128@gmail.com>
2025-03-22 13:41:22 -0500

    changing index

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
+main 
 //Create player
 const player = createPlayer();
 console.log(player);
