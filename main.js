da35b5754717e11007c57d1667fbb5f74e33c8d0
EIannuzzi <eriannuzzi@crimson.ua.edu>
2025-03-22 12:33:06 -0500

    Player object appearance in scene debugging

--- main.js
+++ main.js
@@ -9,21 +9,23 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+//Testing cube
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+const blue = new THREE.Color().setRGB(0, 0, 1);
+//Adding Player
+var player = createPlayer();
+scene.add(player.body);
+
+//Setting camera position
 camera.position.z = 5;
+
+//Animation loop
 renderer.setAnimationLoop( animate );
 
-//VARIABLE DEFINITIONS
-const blue = new THREE.Color().setRGB(0, 0, 1);
-const red = new THREE.Color().setRGB(1, 0, 0);
-const green = new THREE.Color().setRGB(0, 1, 0);
-const yellow = new THREE.Color().setRGB(1, 1, 0);
-const white = new THREE.Color().setRGB(1, 1, 1);
-const black = new THREE.Color().setRGB(0, 0, 0);
 
 //FUNCTION DEFINITIONS
 
@@ -32,26 +34,28 @@ function animate() {
 	renderer.render( scene, camera );
 }
 
-const player = createPlayer();
 function createPlayer(){
-    const player = {
+    var player = {
+		name: "Player1",
         x: 0,
         y: 0,
         z:0,
 		radius: 1,
         color: blue,
-        body: null,
-        speed: 5,
+        body: createNewBody(1, blue, 0, 0, 0),
 
+        speed: 1,
+		kindness: 0,
+		fashion: 0,
+		knowledge: 0,
     }
-	player.body = createNewBody(player.radius, player.color, player.x, player.y, player.z);
-	
+		
     return player;
 }
 
 function createNewBody(radius, color, x, y, z){
     const geometry = new THREE.SphereGeometry(radius, 32, 32);
-    const material = new THREE.MeshStahdardMaterial( { color: color} );
+    const material = new THREE.MeshStandardMaterial( { color: color} );
     const sphere = new THREE.Mesh( geometry, material );
     
     sphere.position.set(x, y, z);
@@ -61,3 +65,11 @@ function createNewBody(radius, color, x, y, z){
 function editPlayerColors(player, color){
     player.color = color;
 }
+
+//VARIABLE DEFINITIONS
+
+const red = new THREE.Color().setRGB(1, 0, 0);
+const green = new THREE.Color().setRGB(0, 1, 0);
+const yellow = new THREE.Color().setRGB(1, 1, 0);
+const white = new THREE.Color().setRGB(1, 1, 1);
+const black = new THREE.Color().setRGB(0, 0, 0);
