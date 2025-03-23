30811fc32451e3bdb658f75f75561a7a89498254
Dezziezuok <dezzie128@gmail.com>
2025-03-23 11:00:02 -0500

    fix color theme

--- main.js
+++ main.js
@@ -21,8 +21,8 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 //Basic color Definitions
-const blue = new THREE.Color().setRGB(0, 0, 1);
-const red = new THREE.Color().setRGB(1, 0, 0);
+const #9370DB = new THREE.Color().setRGB(0, 0, 1);
+const #CD5C5C = new THREE.Color().setRGB(1, 0, 0);
 const green = new THREE.Color().setRGB(0, 1, 0);
 const yellow = new THREE.Color().setRGB(1, 1, 0);
 const white = new THREE.Color().setRGB(1, 1, 1);
